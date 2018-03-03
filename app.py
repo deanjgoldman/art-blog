50b24ac21e5a43ccd8255a02f77d64cad1f63454
Dean Goldman <deanjgoldman@gmail.com>
2018-03-03 15:44:03 -0500

    new basic design

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 from flask_assets import Environment, Bundle
-from flask_sqlalchemy import SQLAlchemy
+# from flask_sqlalchemy import SQLAlchemy
 import os
 
 app = Flask(__name__)
@@ -17,36 +17,30 @@ env.register(
     )
 )
 
-
 UPLOAD_FOLDER = 'static/'
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
-
-db = SQLAlchemy(app)
-
-
-class Blogpost(db.Model):
-    __tablename__ = "blogpost"
-    postid = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.Text)
-    image = db.Column(db.Text)
-    subimages = db.Column(db.Text)
 
 
 @app.route('/')
 def index():
-    context = Blogpost.query.all()
-    return render_template('index.html', context=context)
+    return render_template('index.html')
 
 
-@app.route('/post/<int:post_id>')
-def post(post_id):
-    blogpost = Blogpost.query.get(post_id)
-    title = "../static/" + blogpost.title + "/" + str(blogpost.subimages.split(",")[0])
-    print(title)
-    subimages = blogpost.subimages.split(",")[1:]
-    return render_template('post' + str(post_id) + '.html', title=title, subimages=subimages)
+@app.route('/painting')
+def painting():
+    images = os.listdir(UPLOAD_FOLDER + "/painting")
+    return render_template('painting.html', images=images)
+
+
+@app.route('/drawing')
+def drawing():
+    images = os.listdir(UPLOAD_FOLDER + "/drawing")
+    return render_template('drawing.html', images=images)
+
+
+@app.route('/sculpture')
+def sculpture():
+    images = os.listdir(UPLOAD_FOLDER + "/sculpture")
+    return render_template('sculpture.html', images=images)
 
 
 @app.route('/info')
@@ -57,16 +51,6 @@ def info():
 @app.route('/cv')
 def cv():
     return render_template('cv.html')
-
-
-@app.route('/contact')
-def contact():
-    return render_template('contact.html')
-
-
-@app.route('/blogposts')
-def blogposts():
-    return render_template('index.html')
 
 
 if __name__ == '__main__':
